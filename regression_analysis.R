35d74ccab7f336aaa3c3a667fbc89fd471a2ae32
aittomaki <aittomaki@users.noreply.github.com>
2016-01-28 13:27:33 +0200

    Add reformatting for prot data.

--- regression_analysis.R
+++ regression_analysis.R
@@ -4,10 +4,33 @@
 library(GEOquery)
 
 # path to data
-DATADIR = "~/wrk/dippa-data/"
+DATADIR <- "~/wrk/dippa-data/"
+
+# Constants for analysis
+SIGMA <- 0.1  # used for guaranteeing positivity in normalization
 
 # Load data
-mrna <- getGEO(filename = file.path(DATADIR,"GSE58212_series_matrix.txt.gz"))
-mirna <- getGEO(filename = file.path(DATADIR,"GSE58210_series_matrix.txt.gz"))
-prot <- read.delim(file.path(DATADIR,"Oslo2-RPPA_data.csv"), stringsAsFactors = F)
+eset_mrna <- getGEO(filename = file.path(DATADIR,"GSE58212_series_matrix.txt.gz"))
+eset_mirna <- getGEO(filename = file.path(DATADIR,"GSE58210_series_matrix.txt.gz"))
+df_prot <- read.delim(file.path(DATADIR,"Oslo2-RPPA_data.csv"), stringsAsFactors = F)
+
+# Convert Expression sets to matrices
+mrna <- as.data.frame(exprs(eset_mrna))
+mirna <- as.data.frame(exprs(eset_mirna))
+
+
+
+
+
+### PREPROCESS ####
+
+# Reformat protein data
+rownames(df_prot) <- df_prot$Gene
+# Expand AKT1/2/3-genes in protein data
+i <- match("AKT1/AKT2/AKT3", df_prot$Gene)
+prot <- df_prot[c(1:i, i, i, (i+1):nrow(df_prot)), 3:ncol(df_prot)]
+rownames(prot)[i:(i+2)] <- c("AKT1","AKT2","AKT3")
+
+# Normalization for regression (row-wise)
+#norm.func <- function(row) { ()/() + SIGMA }
 
