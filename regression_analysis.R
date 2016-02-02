c69a2952829084f536af8e0ae2dc012704135384
Viljami Aittomäki <aittomaki@users.noreply.github.com>
2016-02-02 19:18:29 +0200

    Add Anduril output table and fix bugs.

--- regression_analysis.R
+++ regression_analysis.R
@@ -5,13 +5,19 @@ library(rstan)
 
 
 ### For running under Anduril, use this block
-prot <- table1
-gene <- table2
-mirna <- table3
+prot <- as.matrix(table1[,2:ncol(table1)])
+rownames(prot) <- table1[,1]
+gene <- as.matrix(table2[,2:ncol(table2)])
+rownames(gene) <- table2[,1]
+mirna <- as.matrix(table3[,2:ncol(table3)])
+rownames(mirna) <- table3[,1]
 model_file <- param1
 gene_names <- unlist(strsplit(param2, ","))
 mirna_names <- unlist(strsplit(param3, ","))
 
+print(paste("gene",class(gene),paste(dim(gene),collapse=",")))
+print(paste("prot",class(prot),paste(dim(prot),collapse=",")))
+print(paste("mirna",class(mirna),paste(dim(mirna),collapse=",")))
 
 ### For running outside Anduril, uncomment the following
 # # path to data
@@ -30,18 +36,28 @@ mirna_names <- unlist(strsplit(param3, ","))
 ### UNIVARIATE ####
 
 samples <- colnames(prot)
+varnames <- c("beta[1]","gamma")
+valuenames <- c("mean","2.5%","97.5%","n_eff","Rhat")
 fitlist <- list()
+posteriors <- matrix(nrow=0, ncol=length(valuenames))
+names <- matrix(nrow=0, ncol=3)
 
 # Run through all (given) miRNA-protein pairs
 for(g in gene_names) {
     for(m in mirna_names){
-        datalist <- list(N=length(samples), J=1, P=as.numeric(prot[g,samples]), M=t(as.matrix(mirna[m,samples])), G=as.numeric(gene[g,samples]))
+        datalist <- list(N=length(samples), J=1, P=as.numeric((prot[g,samples])), M=as.matrix(mirna[m,samples]), G=as.numeric(gene[g,samples]))
         fit <- stan(file=model_file, data=datalist)
         fitlist <- c(fitlist, list(fit))
         names(fitlist)[length(fitlist)] <- paste(g,m,sep="_")
+        post_new <- summary(fit)$summary[varnames, valuenames]
+        posteriors <- rbind(posteriors, post_new)
+        names_new <- cbind(rep(g,length(varnames)),rep(m,length(varnames)),varnames)
+        names <- rbind(names, names_new)
     }
 }
+table.out <- as.data.frame(cbind(names, posteriors))
+names(table.out) <- c("Gene","miRNA","coef",valuenames)
 
 # Extract the miRNA coefficient and interval for first model
-beta1 <- summary(fitlist[[1]])$summary["beta[1]",c("mean","2.5%","97.5%")]
+#beta1 <- summary(fitlist[[1]])$summary[c("beta[1]","gamma"),c("mean","2.5%","97.5%","n_eff","Rhat")]
 
