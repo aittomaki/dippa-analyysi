f73e24fa2b6b6b643f5030856aed8edd3ae55fea
Viljami Aittomäki <aittomaki@users.noreply.github.com>
2016-02-19 14:30:55 +0200

    Update regression slurm script to latest version.

--- triton/regression_all.sh
+++ triton/regression_all.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --time=0-01:00:00 --mem-per-cpu=1000
+#SBATCH --time=0-01:00:00 --mem-per-cpu=3000
 #SBATCH -p short
 #SBATCH -o log/regression-%a.out
-#SBATCH --array=1
+#SBATCH --array=3-105
 
 ## Bash script for running Bayesian regression analysis models on Triton
 
