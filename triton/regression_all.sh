c6df4969023feaecdb98abf9554b6191d98ac696
Viljami Aittomäki <aittomaki@users.noreply.github.com>
2016-02-29 15:41:25 +0200

    Updated paths for sbatch regression script.

--- triton/regression_all.sh
+++ triton/regression_all.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #SBATCH --time=0-01:00:00 --mem-per-cpu=3000
 #SBATCH -p short
-#SBATCH -o log/regression-%a.out
-#SBATCH --array=3-105
+#SBATCH -o /triton/work/jaittoma/dippa-analyysi/log/regression-%a.out
+#SBATCH --array=1-105
 
 ## Bash script for running Bayesian regression analysis models on Triton
 
+cd $TMPDIR
 #module load R/3.1.1-openblas
-Rscript --vanilla regression_multi_triton.R $SLURM_ARRAY_TASK_ID
+Rscript --vanilla /triton/work/jaittoma/dippa-analyysi/triton/regression_multi_triton.R $SLURM_ARRAY_TASK_ID
