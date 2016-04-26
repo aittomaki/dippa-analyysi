ae93c3a8986c0c2e445274d80424fe854a409cbf
Viljami Aittomäki <aittomaki@users.noreply.github.com>
2016-04-26 17:02:37 +0300

    Fix bugs, add foldwise MLPD version of calculations.

--- process_varsel_results.R
+++ process_varsel_results.R
@@ -2,7 +2,7 @@ FILEDIR <- "/home/viljami/wrk/tritonfake/"
 
 # Load necessary libraries
 library(bayesboot)
-libray(HDInterval)
+library(HDInterval)
 library(ggplot2)
 
 # Load data
@@ -13,40 +13,40 @@ load(file.path(FILEDIR,"dippa-analyysi","execute",paste(nom,".rda",sep="")))
 credible.level <- 0.95
 a <- (1-credible.level)/2
 Q <- qnorm(1-a)
-n_boot <- 10000
+n_boot <- 5000
 
 # Compute reference MLPD's
+mlpd.cv.0 <- mean(mlpd[,1])
+mlpd.cv.full <- mean(mlpd.full)
 mlpd.0 <- mean(lpd[,1])
 mlpd.full <- mean(lpd.full)
 U <- 0.05*(mlpd.0-mlpd.full) # decision limit for variable number to choose
 
 # Compute MLPD and credible intervals for submodels
 # Use Bayesian bootstrap to compute posterior interval
-bb <- function(x, n_boot=n_boot, credMass=credible.level) {
-	babo <- bayesboot(x, weighted.mean, R=n_boot, use.weights=T)
+bb <- function(x, nboot=n_boot, credMass=credible.level) {
+	babo <- bayesboot(x, weighted.mean, R=nboot, use.weights=T)
 	highdi <- hdi(babo, credMass=credMass)
 }
 d.mlpd <- apply(lpd, 2, mean)
 credint <- apply(lpd, 2, bb)
-se <- sapply(lpd, 2, function(x) sqrt(var(x)/length(x)))
-se <-  rbind(d.mlpd-Q*se, d.mlpd+Q*se)
+se <- apply(lpd, 2, function(x) sqrt(var(x)/length(x)))
+se <- rbind(d.mlpd-Q*se, d.mlpd+Q*se)
 d.mlpd <- t(rbind(d.mlpd, credint, se))
 # Compute delta MLPD, change into a data frame
 d.mlpd <- d.mlpd-mlpd.full
 d.mlpd <- as.data.frame(d.mlpd)
 names(d.mlpd) <- c("dMLPD","hdi.lower","hdi.upper","se.lower","se.upper")
-mlpd$n <- 1:nrow(mlpd)
+d.mlpd$n <- 1:nrow(d.mlpd)
 
 # Get chosen number of variables to include in final model
-n.chosen <- min(which(mlpd$lower > U))
+n.chosen <- min(which(d.mlpd$lower > U))
 print(n.chosen)
 
-# Save output
-save(mlpd,file=file.path(FILEDIR,"dippa-analyysi","execute",paste(nom,"-mlpd",".rda",sep="")))
 
 # Make a plot of dMLPD vs num of vars in submodel
 theme_set(theme_bw())
-g <- ggplot(mlpd, aes(n,dMLPD))
+g <- ggplot(d.mlpd, aes(n,dMLPD))
 g <- g + geom_hline(yintercept = 0, linetype="dashed")
 g <- g + geom_hline(yintercept = U, color="red")
 g <- g + geom_errorbar(aes(ymin=hdi.lower, ymax=hdi.upper), color="gray", width=0)
@@ -55,8 +55,52 @@ g <- g + geom_line()
 g <- g + ggtitle(nom)
 ggsave(file.path(FILEDIR,"dippa-analyysi","execute",paste(nom,".png",sep="")), g)
 
+
+
+
+
+
+## COMPUTE THE FOLD-WISE MLPD VERSION TO COMPARE!
+U.cv <- 0.05*(mlpd.cv.0-mlpd.cv.full)
+d.mlpd.cv <- apply(mlpd, 2, mean)
+credint.cv <- apply(mlpd, 2, bb)
+se.cv <- apply(mlpd, 2, function(x) sqrt(var(x)/length(x)))
+se.cv <- rbind(d.mlpd.cv-Q*se.cv, d.mlpd.cv+Q*se.cv)
+d.mlpd.cv <- t(rbind(d.mlpd.cv, credint.cv, se.cv))
+# Compute delta MLPD, change into a data frame
+d.mlpd.cv <- d.mlpd.cv-mlpd.full.cv
+d.mlpd.cv <- as.data.frame(d.mlpd.cv)
+names(d.mlpd.cv) <- c("dMLPD","hdi.lower","hdi.upper","se.lower","se.upper")
+d.mlpd.cv$n <- 1:nrow(d.mlpd.cv)
+
+# Get chosen number of variables to include in final model
+n.chosen.cv <- min(which(d.mlpd.cv$lower > U))
+print(n.chosen.cv)
+
+# Make a plot of dMLPD vs num of vars in submodel
+theme_set(theme_bw())
+g <- ggplot(d.mlpd.cv, aes(n,dMLPD))
+g <- g + geom_hline(yintercept = 0, linetype="dashed")
+g <- g + geom_hline(yintercept = U, color="red")
+g <- g + geom_errorbar(aes(ymin=hdi.lower, ymax=hdi.upper), color="gray", width=0)
+g <- g + geom_errorbar(aes(ymin=se.lower, ymax=se.upper), color="red", width=0)
+g <- g + geom_line()
+g <- g + ggtitle(paste(nom,"fold-wise MLPD's")
+ggsave(file.path(FILEDIR,"dippa-analyysi","execute",paste(nom,"-cv.png",sep="")), g)
+
+
+
+
+
+
+
+# Save output
+save(d.mlpd,d.mlpd.cv,file=file.path(FILEDIR,"dippa-analyysi","execute",paste(nom,"-mlpd",".rda",sep="")))
+
+
+
 # Plot an example of the BB distribution
 babo <- bayesboot(lpd[,4], weighted.mean, R=n_boot, use.weights=T))
-png(file.path(FILEDIR,"dippa-analyysi","execute",paste(nom,"-BBdistr_m4",".png",sep=""))))
+png(file.path(FILEDIR,"dippa-analyysi","execute",paste(nom,"-BBdistr_m4.png",sep=""))))
 print(plot(babo))
 dev.off()
