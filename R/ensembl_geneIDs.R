efd3a63be3191346c71fadf58ee1960f248c4d90
Viljami Aittomäki <aittomaki@users.noreply.github.com>
2016-05-13 13:39:33 +0300

    Move R files to subdir, add script for ensembl IDs.
    Creates a new R subdir for R script files. Also
    adds an R script for downloading ensembl IDs from
    biomaRt.

---
+++ R/ensembl_geneIDs.R
@@ -0,0 +1,17 @@
+## Getting all gene and protein IDs from Ensembl
+
+library(biomaRt)
+
+mart <- useMart("ENSEMBL_MART_ENSEMBL", dataset = "hsapiens_gene_ensembl")
+
+filts <- listFilters(mart, what=c("name","description","options","fullDescription","type","operation"))
+atts <- listAttributes(mart)
+
+chromosomes <- c(1:22, "X", "Y")
+
+allgenes <- getBM(filters="chromosome_name", values=chromosomes, attributes=c("ensembl_gene_id","chromosome_name"), mart=mart)
+protids <- getBM(filters="chromosome_name", values=chromosomes, attributes=c("ensembl_peptide_id","chromosome_name"), mart=mart)
+protgenes <- getBM(filters="chromosome_name", values=chromosomes, attributes=c("ensembl_gene_id","ensembl_peptide_id","chromosome_name"), mart=mart)
+#protgenes <- getBM(filters="with_protein_id", values=TRUE, attributes=c("ensembl_gene_id"), mart=mart)
+
+codinggenes <- getBM(filters="ensembl_peptide_id", values=protids$ensembl_peptide_id, attributes=c("ensembl_gene_id"), mart=mart)
