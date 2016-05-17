4ec203aca70102043a1a62afb89f4ecb1144fa37
Viljami Aittomäki <aittomaki@users.noreply.github.com>
2016-05-17 16:06:01 +0300

    Add output of geneID tables into tmp dir.

--- R/ensembl_geneIDs.R
+++ R/ensembl_geneIDs.R
@@ -15,3 +15,6 @@ protgenes <- getBM(filters="chromosome_name", values=chromosomes, attributes=c("
 #protgenes <- getBM(filters="with_protein_id", values=TRUE, attributes=c("ensembl_gene_id"), mart=mart)
 
 codinggenes <- getBM(filters="ensembl_peptide_id", values=protids$ensembl_peptide_id, attributes=c("ensembl_gene_id"), mart=mart)
+
+write.table(protids, file="~/tmp/ensembl_ids/ensembl_protein_ids.csv", row.names=F, quote=F)
+write.table(codinggenes, file="~/tmp/ensembl_ids/ensembl_coding_genes.csv", row.names=F, quote=F)
