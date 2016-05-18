d8ac3d183af28168226062ae5163323fd35c2c84
Viljami Aittomäki <aittomaki@users.noreply.github.com>
2016-05-18 21:21:35 +0300

    Absolute output path, no tmpdir.
    Changes output file path to written out one instead
    of using WRKDIR var, which didn't seem to work.
    No cd to TMPDIR since that doesn't seem to exist.

--- triton/varsel_cv.sh
+++ triton/varsel_cv.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 #SBATCH --time=8:00:00 --mem-per-cpu=3000
-#SBATCH -o $WRKDIR/dippa-analyysi/log/CV-%a.out
+#SBATCH -o /scratch/work/jaittoma/dippa-analyysi/log/CV-%a.out
 #SBATCH -p batch
 #SBATCH --array=8
 
 ## Bash script for running cross-validated variable selection on Triton
 
 #newgrp triton-users #opens new shell, not good
-cd $TMPDIR
 module load R/3.2.4-foss-2016a
 Rscript $WRKDIR/dippa-analyysi/triton/search_cv.R $SLURM_ARRAY_TASK_ID
 
