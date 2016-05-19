2743bc19c3b8b2b81ececa46b852abf2765f1b7d
Viljami Aittomäki <aittomaki@users.noreply.github.com>
2016-05-19 15:43:41 +0300

    Add code for processign miRNA interactions.
    Code for processing interacting miRNAs downloaded
    from miRWalk. Genes have to be sampled and split,
    because miRWalk only accepts small input and
    there is no program interface so has to be done manually.

--- R/ensembl_geneIDs.R
+++ R/ensembl_geneIDs.R
@@ -18,3 +18,32 @@ codinggenes <- getBM(filters="ensembl_peptide_id", values=protids$ensembl_peptid
 
 write.table(protids, file="~/tmp/ensembl_ids/ensembl_protein_ids.csv", row.names=F, quote=F)
 write.table(codinggenes, file="~/tmp/ensembl_ids/ensembl_coding_genes.csv", row.names=F, quote=F)
+
+
+
+
+#### THE FOLLOWING IS FOR GETTING INTERACTIONS FROM miRWalk FOR CODING GENES ##########
+
+# Take a 200 gene sample from the coding genes
+samplesize <- 1000
+gene_sample <- data.frame(ensembl_gene_id=sample(codinggenes[,1], samplesize))
+# Write sample out in 50 gene batches
+for(i in 1:ceiling(samplesize/50)) {
+    ind <- seq(i,samplesize,ceiling(samplesize/50))
+    write.table(gene_sample[ind,], file=paste("~/tmp/ensembl_ids/gene_sample",i,".txt",sep=""), row.names=F, quote=F)
+}
+
+# Load interactions back in (downloaded manually from miRWalk website
+# use dplyr for help
+library(dplyr)
+inter <- list()
+for(i in 1:ceiling(samplesize/50))
+    inter[[i]] <- read.delim(paste("~/tmp/ensembl_ids/genemirna",i,".csv",sep=""))
+inter <- do.call(rbind, inter)
+# Check number of genes with no interactions
+n_no_inter <- samplesize - length(unique(inter$Gene))
+# Compute number of interacting miRNAs per gene
+mirnas_per_gene <- inter %>% group_by(Gene) %>% summarise(n_distinct(miRNA)) %>% select(2) %>% unlist(use.names=F)
+# Add zeros
+mirnas_per_gene <- c(mirnas_per_gene, rep(0,n_no_inter))
+summary(mirnas_per_gene)
