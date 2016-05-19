570c742ee729356b6e03c15684fb9ef16652b5a5
Viljami Aittomäki <aittomaki@users.noreply.github.com>
2016-05-19 15:46:20 +0300

    Change to correct R version and add parameters for rstan.
    Loads correct R module and now has parameters
    for Rscript that are required for rstan to work.

--- triton/varsel_cv.sh
+++ triton/varsel_cv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --time=8:00:00 --mem-per-cpu=3000
+#SBATCH --time=4:00:00 --mem-per-cpu=3000
 #SBATCH -o /scratch/work/jaittoma/dippa-analyysi/log/CV-%a.out
 #SBATCH -p batch
 #SBATCH --array=8
@@ -7,6 +7,6 @@
 ## Bash script for running cross-validated variable selection on Triton
 
 #newgrp triton-users #opens new shell, not good
-module load R/3.2.4-foss-2016a
-Rscript $WRKDIR/dippa-analyysi/triton/search_cv.R $SLURM_ARRAY_TASK_ID
+module load R/3.2.4-goolf-triton-2016a
+Rscript --default-packages=methods,utils $WRKDIR/dippa-analyysi/triton/search_cv.R $SLURM_ARRAY_TASK_ID
 
