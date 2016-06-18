22e38213787f0d4693fa196c0204729f9a80d45e
Viljami Aittomäki <aittomaki@users.noreply.github.com>
2016-06-18 18:19:57 +0300

    Add output of projected weights in projection.
    Also remove some printing.

--- triton/projection.R
+++ triton/projection.R
@@ -1,11 +1,11 @@
-# 
+#
 # functions for performing the projection predictive variable selection
 # for linear Gaussian model.
 #
 
 lm_proj <- function(w,sigma2,x,indproj) {
-	
-	# assume the intercept term is stacked into w, and x contains 
+
+	# assume the intercept term is stacked into w, and x contains
 	# a corresponding vector of ones. returns the projected samples
 	# and estimated kl-divergence.
 
@@ -17,10 +17,10 @@ lm_proj <- function(w,sigma2,x,indproj) {
 	fit <- x %*% w # fit of the full model
 	wp <- solve(t(xp) %*% xp, t(xp) %*% fit)
 	sigma2p <- sigma2 + colMeans((fit - xp %*% wp)^2)
-	
+
 	# this is the estimated kl-divergence between the full and projected model
 	kl <- mean(0.5*log(sigma2p/sigma2))
-	
+
 	# reshape wp so that it has same dimensionality as x, and zeros for
 	# those variables that are not included in the projected model
 	d <- dim(w)[1]
@@ -28,29 +28,28 @@ lm_proj <- function(w,sigma2,x,indproj) {
 	wptemp <- matrix(0, d, S)
 	wptemp[indproj,] <- wp
 	wp <- wptemp
-	
+
 	return(list(w=wp, sigma2=sigma2p, kl=kl))
 }
 
 
 lm_fprojsel <- function(w, sigma2, x, MAX_VARS) {
 
-	# forward variable selection using the projection
-	print("Starting variable selection...")
+	# forward variable selection using projection
+    kl <- rep(0,MAX_VARS)
+    sigma2p <- rep(0,MAX_VARS)
+    wp <- array(0, dim=c(dim(w),MAX_VARS))
 	d = dim(x)[2]
 	chosen <- 1:2 # chosen variables, start from the model with the intercept and gene term
 	notchosen <- setdiff(1:d, chosen)
-	
-	# start from the model having only the intercept term
-	kl <- rep(0,MAX_VARS)
+
+	# start from model with only the intercept term
 	kl[1] <- lm_proj(w,sigma2,x,1)$kl
-	# add gene term
+	# add gene variable
 	kl[2] <- lm_proj(w,sigma2,x,1:2)$kl
 
-	# start adding miRNA-variables one at a time
+	# start adding miRNA variables one at a time
 	for (k in 3:MAX_VARS) {
-		if((k %% 20) == 0)
-			print(sprintf("%d vars included",k))
 		nleft <- length(notchosen)
 		val <- rep(0, nleft)
 
@@ -64,8 +63,11 @@ lm_fprojsel <- function(w, sigma2, x, MAX_VARS) {
 		imin <- which.min(val)
 		chosen <- c(chosen, notchosen[imin])
 		notchosen <- setdiff(1:d, chosen)
-	
+
 		kl[k] <- val[imin]
+		proj <- lm_proj(w,sigma2,x,imin)
+        wp[,,k] <- proj$w
+        sigma2p <- proj$sigma2
 	}
-	return(list(chosen=chosen, kl=kl))
+	return(list(chosen=chosen, kl=kl, sigma2=sigma2p, w=wp))
 }
