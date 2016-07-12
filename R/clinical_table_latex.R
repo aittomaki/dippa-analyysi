97c52b4288c91285336122abb5b1ebb0958c9160
Viljami Aittomäki <aittomaki@users.noreply.github.com>
2016-07-12 22:44:34 +0300

    Better rounding of table numbers.

--- R/clinical_table_latex.R
+++ R/clinical_table_latex.R
@@ -24,7 +24,7 @@ prettyTable <- function(x, num_cols=NULL) {
         num_cols <- names(x)[sapply(x, is.numeric)]
 
     ## Do num cols first
-    d_num <- ldply(x[,num_cols,drop=F], function(x) c(round(median(x,na.rm=T),1),min(x,na.rm=T),max(x,na.rm=T),sum(is.na(x))))
+    d_num <- ldply(x[,num_cols,drop=F], function(x) c(median(x,na.rm=T),min(x,na.rm=T),max(x,na.rm=T),sum(is.na(x))))
     names(d_num) <- c("Variable","Median","Min","Max","Missing")
     d_num[,"Range"] <- paste(d_num[,"Min"], "--", d_num[,"Max"], sep=" ")
     d_num <- d_num[,c("Variable","Median","Range","Missing")]
@@ -35,7 +35,7 @@ prettyTable <- function(x, num_cols=NULL) {
     ## Other cols
     i <- !(names(x) %in% num_cols)
     # Transform to counts
-    d <- ldply(x[,i,drop=F], function(x) t(rbind(names(table(x,useNA="ifany")),table(x,useNA="ifany"),paste0(round(prop.table(table(x,useNA="ifany"))*100,1),"%"))))
+    d <- ldply(x[,i,drop=F], function(x) t(rbind(names(table(x,useNA="ifany")),table(x,useNA="ifany"),paste0(round(prop.table(table(x,useNA="ifany"))*100,0),"%"))))
 
     # Spaces for variable names
     d[,1] <- ifelse(duplicated(d[,1]), "", d[,1])
@@ -52,13 +52,13 @@ prettyTable <- function(x, num_cols=NULL) {
 tables <- prettyTable(table1, num_cols=numerical_cols)
 
 # Make a LaTeX fragment of first table piece
-ltable1 <- xtable(tables[[1]], caption=caption, align="lllrr")
+ltable1 <- xtable(tables[[1]], caption=caption, align="lllrr", digits=0)
 ltable1 <- print(ltable1, caption.placement = "top", print.results=F, include.rownames=F)
 # Remove table ending
 ltable1 <- sub("   \\hline\n\\end{tabular}\n\\end{table}\n","", ltable1, fixed=T)
 
 # Make a LaTeX fragment of second table piece
-ltable2 <- xtable(tables[[2]])
+ltable2 <- xtable(tables[[2]], digits=0)
 ltable2 <- print(ltable2, print.results=F, include.rownames=F)
 # Remove table beginning
 ltable2 <- sub("\\begin{table}[ht]\n\\centering\n\\begin{tabular}{llll}\n","", ltable2, fixed=T)
